fee1cf85275c4d9207fff01e5d482181f9dad557
soyoon1 <20210841@duksung.ac.kr>
2022-10-06 14:07:27 +0900

    포스트 목록/상세 페이지에 대표이미지 출력하기

--- myDjangoPrj1/urls.py
+++ myDjangoPrj1/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [     # IP 주소/
     path('admin/', admin.site.urls),    # IP 주소/admin
     path('blog/', include('blog.urls')),    # IP 주소/blog
     path('', include('single_pages.urls'))  # IP 주소/
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
