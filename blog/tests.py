d5378a4d3918c84f670a7bbfe61669a0d1cc36d9
soyoon1 <20210841@duksung.ac.kr>
2022-10-18 16:53:58 +0900

    포스트 상세 페이지 테스트하기

--- blog/tests.py
+++ blog/tests.py
@@ -53,3 +53,11 @@ class  TestView(TestCase):
         navbar = soup.nav
         self.assertIn('Blog', navbar.text)
         self.assertIn('AboutMe', navbar.text)
+
+        self.assertIn(post_001.title, soup.title.text)
+
+        main_area=soup.find('div', id='main-area')
+        #post_area=soup.find('div', id='post-area')  한번 찾고 또 처음으로 가서 다시 찾음
+        post_area = main_area.find('div', id='post-area')
+        self.assertIn(post_001.title, post_area.text)
+        self.assertIn(post_001.content, post_area.text)
