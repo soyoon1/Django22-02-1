afd632a807d30beaca0506a639916ce0250c370c
soyoon1 <20210841@duksung.ac.kr>
2022-11-24 16:34:56 +0900

    검색 기능 구현하기

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [ # IP주소/blog/
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('category/<str:slug>/', views.category_page),         # 괄호 안에 첫 번째가 url이고 두번째가 처리할 함수 호출 # IP주소/blog/category/slug
     path('tag/<str:slug>/', views.tag_page),     # IP주소/blog/tag/slug
+    path('search/<str:q>/',views.PostSearch.as_view())
 
 
     #path('', views.index),  # IP주소/blog
